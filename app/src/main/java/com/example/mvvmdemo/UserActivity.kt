9c960b321a3a1d7f0b494cf396cdd2c5d460f0ad
sharker <bigworld51931@126.com>
2020-09-12 23:22:29 +0800

    1.Palette以及kotlin协程的使用

--- app/src/main/java/com/example/mvvmdemo/UserActivity.kt
+++ app/src/main/java/com/example/mvvmdemo/UserActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mvvmdemo
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
 import com.example.mvvmdemo.databinding.ActivityUserBinding
 import com.example.mvvmdemo.entity.UserInfo
 import kotlinx.android.synthetic.main.activity_user.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
 class UserActivity : AppCompatActivity() {
 
@@ -14,8 +19,25 @@ class UserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val userActivityMainBinding: ActivityUserBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_user)
+
+        val bitmap : Bitmap = runBlocking(Dispatchers.IO) {
+            Glide.with(this@UserActivity).asBitmap().load("http://img2.cache.netease.com/auto/2016/7/28/201607282215432cd8a.jpg").submit().get()
+        }
+        val palette  = Palette.from(bitmap).generate()
+        val mutedColor = palette.getVibrantColor(Color.BLACK)
+        //val mutedColor = palette.getDarkMutedColor(Color.BLACK)
+        //val mutedColor = palette.getLightMutedColor(Color.BLACK)
+        //val mutedColor = palette.getMutedColor(Color.BLACK)
+        //val mutedColor = palette.getDominantColor(Color.BLACK)
+        //val mutedColor = palette.getDarkVibrantColor(Color.BLACK)
+
         val userInfo =
-            UserInfo("王老五", "http://img2.cache.netease.com/auto/2016/7/28/201607282215432cd8a.jpg")
+            UserInfo(
+                "王老五",
+                mutedColor,
+                "http://img2.cache.netease.com/auto/2016/7/28/201607282215432cd8a.jpg"
+            )
         userActivityMainBinding.user = userInfo
+
     }
 }
--- app/src/main/java/com/example/mvvmdemo/entity/UserInfo.kt
+++ app/src/main/java/com/example/mvvmdemo/entity/UserInfo.kt
@@ -1,14 +1,29 @@
 package com.example.mvvmdemo.entity
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.os.Handler
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.BindingAdapter
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
-data class UserInfo(val userName: String, val userFace: String) {
+data class UserInfo(val userName: String, val textColor : Int, val userFace: String) {
+
     companion object {
         @BindingAdapter("bind:userFace")
         @JvmStatic
         fun getInternetImage(appCompatImageView: AppCompatImageView, userFace: String) {
+
             Glide.with(appCompatImageView.context).load(userFace).into(appCompatImageView)
+        }
+
+        @BindingAdapter("bind:textColor")
+        @JvmStatic
+        fun setTextColor(textView: AppCompatTextView, textColor: Int) {
+            textView.setTextColor(textColor)
         }
     }
 }
